13d7c3192d6f6c8b0c9cbefc0ac90bb284367372
Nagel <pedronagelalves@gmail.com>
2025-05-28 20:20:14 -0300

    alteração dos json do backend

--- backEnd-pizzaria/obj/Debug/net8.0/backEnd-pizzaria.AssemblyInfo.cs
+++ backEnd-pizzaria/obj/Debug/net8.0/backEnd-pizzaria.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backEnd-pizzaria")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2908b6230405f49041a418996002971ff9b31f60")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+78b205d77b0af2ac28fbc877cbbe619e064c8579")]
 [assembly: System.Reflection.AssemblyProductAttribute("backEnd-pizzaria")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backEnd-pizzaria")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
